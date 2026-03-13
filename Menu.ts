e490ff1810424f0ee458657cfee24251d0d63ce8
Beatriz Braga <beatrizsagrado9@gmail.com>
2026-03-13 17:10:08 -0300

    feat: metodos sacar, depositar, transferir

--- Menu.ts
+++ Menu.ts
@@ -14,7 +14,7 @@ import { ContaController } from './src/controller/ContaController';
 export function main() {
 
     //variaveis e constantes
-    let opcao, numero, agencia, tipo, saldo, limite, aniversario: number;
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario, valor, numeroDestino: number;
     let titular: string;
     const tiposContas = ['Conta Corrente', 'Conta Poupanca'];
 
@@ -179,17 +179,31 @@ export function main() {
                 break;
             case 6:
                 console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
-
+                console.log("Digite o número da Conta: ");
+                numero = readlinesync.questionInt("");
+                console.log("\nDigite o valor do Saque (R$): ");
+                valor = readlinesync.questionFloat("");
+                contas.sacar(numero, valor);
                 keyPress()
                 break;
             case 7:
                 console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
-
+                console.log("Digite o número da Conta: ");
+                numero = readlinesync.questionInt("");
+                console.log("\nDigite o valor do Depósito (R$): ");
+                valor = readlinesync.questionFloat("");
+                contas.depositar(numero, valor);
                 keyPress()
                 break;
             case 8:
                 console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
-
+                console.log("Digite o número da Conta de Origem: ");
+                numero = readlinesync.questionInt("");
+                console.log("Digite o número da Conta de Destino: ");
+                numeroDestino = readlinesync.questionInt("");
+                console.log("\nDigite o valor do Depósito (R$): ");
+                valor = readlinesync.questionFloat("");
+                contas.transferir(numero, numeroDestino, valor);
                 keyPress()
                 break;
             default:
